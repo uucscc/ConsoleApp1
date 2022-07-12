cea618717044a602adee5d63ae605e7f532ba374
泉 井 <2730551617@qq.com>
2022-07-12 18:16:08 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0,b=0;
+            for (int i = 0; i <100; i++)
+            {
+                if (i%2==0)
+                {
+                    a += i * 2 + 1;
+                    if (i < 100)
+                    {
+                        b = i * 2 + 1;
+                    }
+
+                }
+                if (i%2==1)
+                {
+                    a += -(i * 2 + 1);
+                }
+                if (i<100)
+                {
+                    b = i * 2 + 1;
+                }
+                
+                
+            }
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
